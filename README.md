38c176fe4ce1575d961bb6ceb5add7c419f788db
patricioquiroga35 <patricioquiroga35@gmail.com>
2025-11-27 12:30:26 -0300

    cambio

--- README.md
+++
@@ -1,2 +0,0 @@
-# Liga-Juje-a
-Sistema para administracion de una liga de futbol
--- src/main/java/com/patricio/springboot/app/DemoApplication.java
+++ src/main/java/com/patricio/springboot/app/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.patricio.springboot.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
