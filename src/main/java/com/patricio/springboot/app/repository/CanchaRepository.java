a81f753300fa473869aeafe0239e7c360a829cd7
patricioquiroga35 <patricioquiroga35@gmail.com>
2025-12-03 23:03:43 -0300

    Arregle el metodo para que solo liste canchas que esten activas

--- src/main/java/com/patricio/springboot/app/repository/CanchaRepository.java
+++ src/main/java/com/patricio/springboot/app/repository/CanchaRepository.java
@@ -2,8 +2,13 @@ package com.patricio.springboot.app.repository;
 
 import com.patricio.springboot.app.entity.Cancha;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface CanchaRepository extends JpaRepository<Cancha, Integer>{
      Optional<Cancha> findById(Long id);
+
+    List<Cancha> findAllByEstadoTrue();
 }
