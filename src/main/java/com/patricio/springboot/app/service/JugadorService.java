1d24b7b161fe3ded165f81d4ee517cf8a7513201
patricioquiroga35 <patricioquiroga35@gmail.com>
2025-12-07 18:09:22 -0300

    Agregue nuevos metodos a jugadorescontroller

--- src/main/java/com/patricio/springboot/app/service/JugadorService.java
+++ src/main/java/com/patricio/springboot/app/service/JugadorService.java
@@ -1,4 +1,25 @@
 package com.patricio.springboot.app.service;
 
+import com.patricio.springboot.app.entity.Jugador;
+import com.patricio.springboot.app.repository.EquipoRepository;
+import com.patricio.springboot.app.repository.JugadorRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class JugadorService {
+
+    private JugadorRepository jugadorRepository;
+    public JugadorService(JugadorRepository jugadorRepository) {
+        this.jugadorRepository = jugadorRepository;
+    }
+
+
+
+
+
+    public Jugador obtenerJugadorID(Long id) {
+        return jugadorRepository.getById(id);
+    }
 }
