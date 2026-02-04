46bc6b6694055d7a693bd4a3e7038073b2df308d
patricioquiroga35 <patricioquiroga35@gmail.com>
2026-02-04 00:11:57 -0300

    cambio de colores

--- src/main/java/com/patricio/springboot/app/config/SecurityConfig.java
+++ src/main/java/com/patricio/springboot/app/config/SecurityConfig.java
@@ -62,7 +62,8 @@ public class SecurityConfig {
                 Arrays.asList(
                         "http://localhost:5173",
                         "https://*.railway.app", // Permite subdominios de Railway
-                        "https://jujuyligas.vercel.app" // Agregaremos la de Vercel luego
+                        "https://jujuyligas.vercel.app",
+                        "https://www.jujuyligas.live"
                 )
         );
         config.setAllowedMethods(
